bf14abc13a70c79173ffe2251fc565a5158af2d2
root <root@ip-172-31-49-148.ec2.internal>
2016-10-12 04:11:47 +0000

    wp-content/

--- wp-content/plugins/woocommerce-multilingual/inc/class-wcml-cart.php
+++ wp-content/plugins/woocommerce-multilingual/inc/class-wcml-cart.php
@@ -201,6 +201,17 @@ class WCML_Cart
 
     public function translate_cart_subtotal( $cart ) {
 
+        if( isset( $_SERVER['REQUEST_URI'] ) ){
+            //special case: check if attachment loading
+            $attachments = array( 'png', 'jpg', 'jpeg', 'gif', 'js', 'css' );
+
+            foreach( $attachments as $attachment ){
+                if( !empty( preg_match( '/\.'.$attachment.'$/',  $_SERVER['REQUEST_URI'] ) ) ){
+                    return false;
+                }
+            }
+        }
+
         if( apply_filters( 'wcml_calculate_totals_exception', true, $cart ) ){
             $cart->calculate_totals();
         }
