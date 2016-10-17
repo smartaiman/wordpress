07646c6bdaeea4f33ddd36d32b691c4b4f20fbb2
root <root@ip-172-31-50-141.ec2.internal>
2016-10-17 23:19:27 +0000

    ubdate

--- wp-config.php
+++ wp-config.php
@@ -28,7 +28,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', 'wordpress.cluster-cftqt4jaczzg.us-east-1.rds.amazonaws.com');
+define('DB_HOST', 'wordpress-rds.cftqt4jaczzg.us-east-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
